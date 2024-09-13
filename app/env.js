52a4b6fe7438b1a8b35ba298ddb9e816a6cd9b03
harshad <harshad@yaper.co>
2024-09-13 10:19:40 +0530

    yaper public setup

--- app/env.js
+++ app/env.js
@@ -1,20 +1,4 @@
 export const APP_VARIABLES = {
-  production: {
-    env: "production",
-    baseUrl: "https://app.yaper.co",
-    log: false,
-    oneSignalKey: "5440990a-ba73-4cb6-96ec-d3c777f00fdb",
-    segmentKey: "655pjpNrRcTis3zqzpJSUnfv4e2JEt2O",
-    sentryDebug: false,
-    sentryDSN: "https://0cf01f210ca848059d4f7721009bc900@bugs.yaper.co/3",
-  },
-  staging: {
-    env: "staging",
-    baseUrl: "https://sadmin.yaper.co",
-    log: true,
-    oneSignalKey: "9762c2d9-aff9-4be6-ac46-c3791c80bb94",
-    segmentKey: "pKy4EPtpPLaAWOBYKWisyP6eet5xJoIz",
-    sentryDebug: true,
-    sentryDSN: "https://0cf01f210ca848059d4f7721009bc900@bugs.yaper.co/3",
-  },
+  production: {},
+  staging: {},
 };
